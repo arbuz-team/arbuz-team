ae097b13d9c6fe97000db14e0f625a845b57318f
arbuz93 <93.endo@gmail.com>
2017-04-12 14:43:41 +0200

    Change start page.

--- server/main/views.py
+++ server/main/views.py
@@ -4,8 +4,8 @@ from server.arbuz.views import *
 
 class Start(Dynamic_Event_Manager):
 
-    def Manage_Index(self):
-        return self.Render_HTML('main/start.html')
+    def Manage_Content_Ground(self):
+        return JsonResponse({})
 
     @staticmethod
     def Launch(request):
